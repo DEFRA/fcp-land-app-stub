854306f3743279f205bd02ef4a9389eeeb94c93e
John Watson <john.watson1@defra.gov.uk>
2026-09-18 12:34:07 +0100

    Rename var

--- src/plugins/headers.js
+++ src/plugins/headers.js
@@ -7,31 +7,31 @@ export const headers = {
     name: 'headers',
     register: (server, _options) => {
       server.ext('onPreResponse', (request, h) => {
-        const headers = request.response.headers
+        const responseHeaders = request.response.headers
 
-        if (!headers) {
+        if (!responseHeaders) {
           return h.continue
         }
 
-        headers['X-Content-Type-Options'] = 'nosniff'
-        headers['X-Frame-Options'] = 'DENY'
-        headers['X-Robots-Tag'] = 'noindex, nofollow'
-        headers['X-XSS-Protection'] = '1; mode=block'
-        headers['Cross-Origin-Opener-Policy'] = 'same-origin'
-        headers['Cross-Origin-Embedder-Policy'] = 'require-corp'
-        headers['Cross-Origin-Resource-Policy'] = 'same-site'
-        headers['Referrer-Policy'] = 'no-referrer'
-        headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains; preload'
-        headers['Permissions-Policy'] = 'camera=(), geolocation=(), magnetometer=(), microphone=(), payment=(), usb=()'
+        responseHeaders['X-Content-Type-Options'] = 'nosniff'
+        responseHeaders['X-Frame-Options'] = 'DENY'
+        responseHeaders['X-Robots-Tag'] = 'noindex, nofollow'
+        responseHeaders['X-XSS-Protection'] = '1; mode=block'
+        responseHeaders['Cross-Origin-Opener-Policy'] = 'same-origin'
+        responseHeaders['Cross-Origin-Embedder-Policy'] = 'require-corp'
+        responseHeaders['Cross-Origin-Resource-Policy'] = 'same-site'
+        responseHeaders['Referrer-Policy'] = 'no-referrer'
+        responseHeaders['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains; preload'
+        responseHeaders['Permissions-Policy'] = 'camera=(), geolocation=(), magnetometer=(), microphone=(), payment=(), usb=()'
 
         // Disable caching for all routes except the index page and assets
         // This is to prevent browsers from caching sensitive data in the browser history
         // Prevents the back button from displaying sensitive data after the user has signed out
         if (request.path !== '/' && !request.path.startsWith(assetPath)) {
           // Cache-Control must be lower case to avoid conflicts with Hapi's built-in header handling
-          headers['cache-control'] = 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0'
-          headers.Pragma = 'no-cache'
-          headers.Expires = '0'
+          responseHeaders['cache-control'] = 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0'
+          responseHeaders.Pragma = 'no-cache'
+          responseHeaders.Expires = '0'
         }
 
         return h.continue
